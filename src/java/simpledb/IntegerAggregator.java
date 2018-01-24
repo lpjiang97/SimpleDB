34cbf15d31647c59476a3905be9249b186a52f88
Linxing Preston Jiang <prestonj@cs.washington.edu>
2018-01-23 23:51:08 -0800

    Int Aggregate tests passing

--- src/java/simpledb/IntegerAggregator.java
+++ src/java/simpledb/IntegerAggregator.java
@@ -1,11 +1,24 @@
 package simpledb;
 
+import sun.text.normalizer.IntTrie;
+
+import java.util.*;
+
 /**
  * Knows how to compute some aggregate over a set of IntFields.
  */
 public class IntegerAggregator implements Aggregator {
 
     private static final long serialVersionUID = 1L;
+
+    private int gbfield;
+    private Type gbfieldtype;
+    private int afield;
+    private Op what;
+
+    private Map<Field, Integer> groupMap;
+    // only used for AVG aggregate
+    private Map<Field, List<Integer>> avgMap;
 
     /**
      * Aggregate constructor
@@ -23,7 +36,13 @@ public class IntegerAggregator implements Aggregator {
      */
 
     public IntegerAggregator(int gbfield, Type gbfieldtype, int afield, Op what) {
-        // some code goes here
+        this.gbfield = gbfield;
+        this.gbfieldtype = gbfieldtype;
+        this.afield = afield;
+        this.what = what;
+        this.groupMap = new HashMap<>();
+        // can't do avg on the go -- integer division might result in bad value
+        this.avgMap = new HashMap<>();
     }
 
     /**
@@ -34,8 +53,56 @@ public class IntegerAggregator implements Aggregator {
      *            the Tuple containing an aggregate field and a group-by field
      */
     public void mergeTupleIntoGroup(Tuple tup) {
-        // some code goes here
+        // get fields
+        IntField afield = (IntField)tup.getField(this.afield);
+        Field gbfield = tup.getField(this.gbfield);
+        int newValue = afield.getValue();
+
+        if (gbfield.getType() != this.gbfieldtype) {
+            throw new IllegalArgumentException("Given tuple has wrong type");
+        }
+        // get number
+        switch (this.what) {
+            case MIN:
+                if (!this.groupMap.containsKey(gbfield))
+                    this.groupMap.put(gbfield, newValue);
+                else
+                    this.groupMap.put(gbfield, Math.min(this.groupMap.get(gbfield), newValue));
+                break;
+            case MAX:
+                if (!this.groupMap.containsKey(gbfield))
+                    this.groupMap.put(gbfield, newValue);
+                else
+                    this.groupMap.put(gbfield, Math.max(this.groupMap.get(gbfield), newValue));
+                break;
+            case SUM:
+                if (!this.groupMap.containsKey(gbfield))
+                    this.groupMap.put(gbfield, newValue);
+                else
+                    this.groupMap.put(gbfield, this.groupMap.get(gbfield) + newValue);
+                break;
+            case COUNT:
+                if (!this.groupMap.containsKey(gbfield))
+                    this.groupMap.put(gbfield, 1);
+                else
+                    this.groupMap.put(gbfield, this.groupMap.get(gbfield) + 1);
+                break;
+            case AVG:
+                if (!this.avgMap.containsKey(gbfield)) {
+                    List<Integer> l = new ArrayList<>();
+                    l.add(newValue);
+                    this.avgMap.put(gbfield, l);
+                } else {
+                    // reference
+                    List<Integer> l = this.avgMap.get(gbfield);
+                    l.add(newValue);
+                }
+                break;
+            default:
+                throw new IllegalArgumentException("Aggregate not supported!");
+        }
     }
+
 
     /**
      * Create a OpIterator over group aggregate results.
@@ -46,9 +113,91 @@ public class IntegerAggregator implements Aggregator {
      *         the constructor.
      */
     public OpIterator iterator() {
-        // some code goes here
-        throw new
-        UnsupportedOperationException("please implement me for lab2");
+        return new IntAggIterator();
     }
 
+    private class IntAggIterator implements OpIterator {
+
+        private Iterator<Map.Entry<Field, Integer>> it;
+        private Iterator<Map.Entry<Field, List<Integer>>> avgIt;
+        private boolean isAvg;
+        private TupleDesc td;
+
+
+        @Override
+        public void open() throws DbException, TransactionAbortedException {
+            this.it = groupMap.entrySet().iterator();
+            this.isAvg = (what.equals(Op.AVG));
+            if (this.isAvg)
+                this.avgIt = avgMap.entrySet().iterator();
+            else
+                this.avgIt = null;
+            // no grouping
+            if (groupMap.containsKey(null))
+                this.td = new TupleDesc(new Type[] {Type.INT_TYPE}, new String[] {"aggregateVal"});
+            else
+                this.td = new TupleDesc(new Type[] {gbfieldtype, Type.INT_TYPE}, new String[] {"groupVal", "aggregateVal"});
+        }
+
+        @Override
+        public boolean hasNext() throws DbException, TransactionAbortedException {
+            if (this.isAvg)
+                return avgIt.hasNext();
+            return it.hasNext();
+        }
+
+        @Override
+        public Tuple next() throws DbException, TransactionAbortedException, NoSuchElementException {
+            int value = 0;
+            boolean noGroup = false;
+            Field f = null;
+            if (this.isAvg) {
+                Map.Entry<Field, List<Integer>> entry = this.avgIt.next();
+                f = entry.getKey();
+                noGroup = f == null;
+                List<Integer> l = entry.getValue();
+                value = this.sumList(l) / l.size();
+            } else {
+                Map.Entry<Field, Integer> entry = this.it.next();
+                f = entry.getKey();
+                noGroup = f == null;
+                value = entry.getValue();
+            }
+            Tuple rtn = new Tuple(this.td);
+            // no grouping
+            if (noGroup) {
+                rtn.setField(0, new IntField(value));
+            } else {
+                rtn.setField(0, f);
+                rtn.setField(1, new IntField(value));
+            }
+            return rtn;
+        }
+
+        @Override
+        public void rewind() throws DbException, TransactionAbortedException {
+            this.it = groupMap.entrySet().iterator();
+            if (this.isAvg)
+                this.avgIt = avgMap.entrySet().iterator();
+        }
+
+        @Override
+        public TupleDesc getTupleDesc() {
+            return this.td;
+        }
+
+        @Override
+        public void close() {
+            this.it = null;
+            this.avgIt = null;
+            this.td = null;
+        }
+
+        private int sumList(List<Integer> l) {
+            int sum = 0;
+            for (int i : l)
+                sum += i;
+            return sum;
+        }
+    }
 }
